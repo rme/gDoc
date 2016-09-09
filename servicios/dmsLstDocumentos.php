5d63acb09804b8a52e399bb0d8e26f1f7e5c8621
Roberto Morales E <rme.plan.b@gmail.com>
2016-09-09 17:39:22 -0400

    Adiciones a la grabacion del catalogo

--- servicios/dmsLstDocumentos.php
+++ servicios/dmsLstDocumentos.php
@@ -7,17 +7,20 @@ $html = ' <h3>Mis Documentos</h3>
             <th>ID</th>
             <th>VALIDEZ</th>
             <th>TIPO</th>
+            <th>PLANTILLA</th>
             <th>CONTENIDO</th>
             <th>CATALOGO</th>
           </tr>';
-$res = $conn->query("select d.*, td.tdoc_descripcion from dms_documentos d
-                     inner join dms_tipos_documento td on td.tdoc_id = d.doc_tdoc_id
-                     order by d.doc_id desc");
+$res = $conn->query("select d.*, td.tdoc_descripcion, p.plt_descripcion from dms_documentos d
+                      inner join dms_tipos_documento td on td.tdoc_id = d.doc_tdoc_id
+                      inner join dms_plantillas p on p.plt_id = d.doc_plt_id
+                      order by d.doc_id desc");
 foreach($res as $row) {
   $html .= '<tr>
               <td align="right">'.$row["doc_id"].'</td>
               <td align="center">'.$row["doc_validez"].' dias </td>
               <td>'.$row["tdoc_descripcion"].'</td>
+              <td>'.$row["plt_descripcion"].'</td>
               <td>'.$row["doc_contenido"].'</td>
               <td>'.$row["doc_catalogo"].'</td>
             </tr>';
